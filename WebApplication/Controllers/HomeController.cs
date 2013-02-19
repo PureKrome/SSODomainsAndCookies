eb174753e84a805161557b78b5d322a8ae0b7c09
Pure Krome <github@world-domination.com.au>
2013-02-20 09:42:09 +1100

    - Initial commit.

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var indexViewModel = new IndexViewModel
+                                     {
+                                         CookieName = ConfigurationManager.AppSettings["ThisCookieName"]
+                                     };
+
+            if (TempData["IndexViewModelMessage"] != null)
+            {
+                indexViewModel.Message = TempData["IndexViewModelMessage"] as string;
+            }
+
+            // Search for a particular cookie and dump info.
+            var cookie = Request.Cookies[ConfigurationManager.AppSettings["ThisCookieName"]];
+            if (cookie != null)
+            {
+                indexViewModel.Cookie = cookie;
+            }
+
+            return View(indexViewModel);
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult Create(string cookieData)
+        {
+            // Create their cookie.
+            var cookie = new HttpCookie(ConfigurationManager.AppSettings["OtherCookieName"], cookieData)
+                             {
+                                 Expires = DateTime.Now.AddMonths(1),
+                                 Path = "/",
+                                 Domain = ConfigurationManager.AppSettings["OtherCookieDomain"]
+                             };
+            
+            // Note: If the cookie already exists (by name), it's updated/overwritten.
+            Response.SetCookie(cookie);
+
+            TempData["IndexViewModelMessage"] = "Cookie Saved";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication/Models/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace WebApplication.Models
+{
+    public class IndexViewModel
+    {
+        public string Message { get; set; }
+        public string CookieName { get; set; }
+        public HttpCookie Cookie { get; set; }
+    }
+}
