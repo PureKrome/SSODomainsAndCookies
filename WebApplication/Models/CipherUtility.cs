a106e3fe116f64408de7dd8293ced5ed01126aac
RPM1984 <rpmir1@gmail.com>
2013-02-27 12:28:16 +1100

    - fixed

--- WebApplication/Models/CipherUtility.cs
+++ WebApplication/Models/CipherUtility.cs
@@ -10,28 +10,9 @@ namespace WebApplication.Models
         public static string Encrypt<T>(string value, string key, string iv)
             where T : SymmetricAlgorithm, new()
         {
-            //var x = new AesCryptoServiceProvider();
-            //x.KeySize = 128;
-            //x.GenerateIV();
-            //x.GenerateKey();
-
-            //var genB = x.Key;
-            //var genIV = x.IV;
-
-            //var k = Convert.ToBase64String(x.Key);
-            //var v = Convert.ToBase64String(x.IV);
-
-            key = "ZcOLAbbz13RVnQ7uPJKH8w==";
-            iv = "WuH5s2BF/UENerWnrELgfA==";
-
-            var bKey = Convert.FromBase64String(key);
-            var bIV = Convert.FromBase64String(iv);
-
-
             SymmetricAlgorithm algorithm = new T();
-            algorithm.KeySize = 128;
 
-            ICryptoTransform transform = algorithm.CreateEncryptor(bKey, bIV);
+            ICryptoTransform transform = algorithm.CreateEncryptor(Convert.FromBase64String(key), Convert.FromBase64String(iv));
 
             using (var buffer = new MemoryStream())
             {
@@ -50,16 +31,9 @@ namespace WebApplication.Models
         public static string Decrypt<T>(string text, string key, string iv)
             where T : SymmetricAlgorithm, new()
         {
-            key = "ZcOLAbbz13RVnQ7uPJKH8w==";
-            iv = "WuH5s2BF/UENerWnrELgfA==";
-
-            var bKey = Convert.FromBase64String(key);
-            var bIV = Convert.FromBase64String(iv);
-
             SymmetricAlgorithm algorithm = new T();
-            algorithm.KeySize = 128;
 
-            ICryptoTransform transform = algorithm.CreateEncryptor(bKey, bIV);
+            ICryptoTransform transform = algorithm.CreateDecryptor(Convert.FromBase64String(key), Convert.FromBase64String(iv));
 
             using (var buffer = new MemoryStream(Convert.FromBase64String(text)))
             {
